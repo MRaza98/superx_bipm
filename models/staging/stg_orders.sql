a68fcbe9b1a37a4b5f34806ad42e0263b8dbdfc9
MRaza98 <raza.1998@hotmail.com>
2023-12-05 14:51:47 +0100

    first shot at stg orders

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -1,10 +1,12 @@
 
-WITH source AS {{ source('source', 'orders') }}
+WITH source AS 
+
+    (SELECT * FROM {{ source('source', 'orders') }})
 
 SELECT
 
     CAST(id AS INT) AS order_id
-    , CAST(state AS STR) AS order_state
+    , CAST(state AS STRING) AS order_state
     , CAST(timestamp as TIMESTAMP) AS order_timestamp
     , CAST(employee_id AS INT) AS employee_id
     , CAST(retailer_id AS INT) AS retailer_id
