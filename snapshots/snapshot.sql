b2b506bb11b07fbf38630a7855fc13e32988e607
MRaza98 <raza.1998@hotmail.com>
2023-12-14 14:52:31 +0100

    snapshot experimentation

---
+++ snapshots/snapshot.sql
@@ -0,0 +1,17 @@
+{% snapshot snapshot_materials %}
+
+{{ 
+    config(
+        target_database = 'data-warehousing-402518',
+        target_schema = 'staging',
+        unique_key = 'id',
+        
+        strategy = 'timestamp',
+        updated_at = 'timestamp'
+    )
+
+}}
+
+SELECT * FROM {{ source('prod', 'retailers') }}
+
+{% endsnapshot %}
