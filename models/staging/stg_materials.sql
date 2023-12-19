cfdff90dcb0f04fae88b51058c7f90bbfe7a0117
MRaza98 <raza.1998@hotmail.com>
2023-12-19 19:43:03 +0100

    solved 3 data quality issues

--- models/staging/stg_materials.sql
+++
@@ -1,1 +0,0 @@
-SELECT * FROM {{ ref('snapshot_stg_materials') }}
--- models/staging/stg_retailers.sql
+++ models/staging/stg_retailers.sql
@@ -3,9 +3,13 @@ WITH source AS
 
     (SELECT * FROM {{ source('prod', 'retailers') }})
 
-, seed AS
+, retailer_category_mapping AS
 
     (SELECT * FROM {{ ref('retailer_category_mapping') }})
+
+, retailer_country_mapping AS
+
+    (SELECT * FROM {{ ref('retailer_country_mapping') }})
 
 , base AS (
 
@@ -16,12 +20,33 @@ SELECT
     , CAST(name as STRING) AS retailer_name
     , CAST(category AS STRING) AS retailer_category
     , CAST(currency AS STRING) AS retailer_currency
+    , CAST(address AS STRING) AS retailer_address
 
 FROM source
 
 )
 
+, id_refined AS (
+
+    SELECT
+        *
+        , CASE WHEN retailer_id = 101 THEN 83 ELSE retailer_id END AS consolidated_retailer_id
+    FROM base
+
+) 
+
+, address_refined AS (
+
+    SELECT
+        *
+        , TRIM(REGEXP_EXTRACT(retailer_address, r'[^,]+$')) AS retailer_country
+        , TRIM(REGEXP_EXTRACT(retailer_address, r'([^,]+),[^,]+$')) AS retailer_region
+    FROM id_refined
+    
+)
+
 SELECT
-    * EXCEPT (retailer_category)
-FROM base
-JOIN seed USING (retailer_category)
+    * EXCEPT (retailer_category, retailer_country)
+FROM address_refined
+JOIN retailer_category_mapping USING (retailer_category)
+JOIN retailer_country_mapping USING(retailer_country)
