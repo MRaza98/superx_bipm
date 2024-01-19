c570023c360d02d742a192ba23b1fe959efececa
MRaza98 <raza.1998@hotmail.com>
2024-01-20 00:17:41 +0100

    random

---
+++ models/mart/dim_retailers.sql
@@ -0,0 +1,15 @@
+
+WITH base AS 
+
+    (SELECT * FROM {{ ref('stg_retailers') }})
+
+SELECT
+
+    consolidated_retailer_id AS retailer_id
+    , retailer_state
+    , retailer_name
+    , retailer_region
+    , retailer_consolidated_category AS retailer_category
+    , retailer_country_consolidated AS retailer_country
+
+FROM base
