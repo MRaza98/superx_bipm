908b939cd84b1dc9550d56f8d7773a5701baf96b
MRaza98 <raza.1998@hotmail.com>
2024-02-14 12:34:37 +0100

    test commit

--- models/external/product_analysis.sql
+++ models/external/product_analysis.sql
@@ -1,40 +1,36 @@
+WITH orders AS (SELECT * FROM {{ ref('fct_orders') }}),
 
-WITH orders AS 
+products AS (SELECT * FROM {{ ref('dim_products') }}),
 
-    (SELECT * FROM {{ ref('fct_orders') }})
-
-, products AS 
-
-    (SELECT * FROM {{ ref('dim_products') }})
-
-, orders_revenue_per_product AS (
+orders_revenue_per_product AS (
 
     SELECT
 
-        product_id
-        , order_currency
-        , DATE_TRUNC(order_time, DAY) AS order_date
-        , DATE_TRUNC(order_time, MONTH) AS order_month
-        , order_state
-        , COUNT(*) AS n_orders
-        , SUM(price) AS amount
-        , SUM(EUR_price) as EUR_amount
+        product_id,
+        order_currency,
+        DATE_TRUNC(order_time, DAY) AS order_dates,
+        DATE_TRUNC(order_time, MONTH) AS order_month,
+        order_states,
+        COUNT(*) AS n_orders,
+        SUM(price) AS amount,
+        SUM(eur_price) AS eur_amount
 
     FROM orders
     GROUP BY 1, 2, 3, 4, 5
 
-)
+),
 
-, final AS (
+final AS (
 
     SELECT
-        orders_revenue_per_product.*
-        , product_name
-        , product_type
-    
+        orders_revenue_per_product.*,
+        product_name,
+        product_type
+
     FROM orders_revenue_per_product
-    LEFT JOIN products ON products.product_id = orders_revenue_per_product.product_id
-    AND orders_revenue_per_product.order_date BETWEEN products.valid_from AND products.valid_to
+    LEFT JOIN products ON
+        orders_revenue_per_product.product_id = products.product_id
+        AND orders_revenue_per_product.order_date BETWEEN products.valid_from AND products.valid_to
 
 )
 
